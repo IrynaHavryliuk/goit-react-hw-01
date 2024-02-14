abb1aba876fc57f4b5ba862f38e1414c49f50545
IrynaHavryliuk <gis.80@ukr.net>
2024-02-14 11:33:45 -0500

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
-import Profile from '../Profile/Profile.jsx';
+import Profile from '../Profile/Profile';
 import userData from '../userData.json';
 import friends from '../friends.json';
-import FriendList from '../FriendList/FriendList.jsx'
+import FriendList from '../FriendList/FriendList'
 
 
 
